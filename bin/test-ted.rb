e787ba1c6e4a3896af2cdfe7d092eade3da0cc12
Timothy Mecklem <timothy@mecklem.com>
2014-04-24 21:35:42 -0400

    A little tweak here and there

--- bin/test-ted.rb
+++ bin/test-ted.rb
@@ -8,16 +8,13 @@ sp = SerialPort.new ARGV[0], 1200
 
 while true do
   begin
-    byte = sp.readbyte
-  end until !byte.nil? && 0xaa == byte
+    byte = sp.read(1)
+  end until ["aa"] == byte.unpack('H*')
 
-  bytes = [byte]
-  10.times do
-    bytes << sp.readbyte
-  end
+  bytes = "#{byte}#{sp.read(10)}"
+  puts bytes.unpack('H*')
 
-  message = Ted::Recorder::Ted1000Message.new(bytes.pack('C*'))
+  message = Ted::Recorder::Ted1000Message.new(bytes)
   puts message.inspect if message.verify?
 
-  #while true do print "%02x" % ~(sp.readbyte) end
 end
