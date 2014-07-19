2d54f2985d4676eea10b67a009fcd97b58559d70
Timothy Mecklem <timothy@mecklem.com>
2014-07-19 17:45:54 +0000

    Removing influx and setting up for pi (less chatty)

--- bin/test-plotwatt-write.rb
+++ bin/test-plotwatt-write.rb
@@ -2,7 +2,7 @@
 
 require 'serialport'
 require 'ted/recorder'
-require 'influxdb'
+#require 'influxdb'
 require 'rest_client'
 
 username = 'ted'
@@ -12,7 +12,7 @@ api_key = ARGV[1]
 meter_id = ARGV[2]
 
 sp = SerialPort.new ARGV[0], 1200
-influxdb = InfluxDB::Client.new database, :username => username, :password => password
+#influxdb = InfluxDB::Client.new database, :username => username, :password => password
 
 plotwatt_messages = []
 
@@ -26,18 +26,18 @@ while true do
   message = Ted::Recorder::Ted1000Message.new(bytes)
   if message.verify?
 
-    puts "received and verified #{message.inspect}"
-    begin
-      influxdb.write_point("power", {:value => message.power})
-      influxdb.write_point("voltage", {:value => message.voltage})
-      puts "Wrote power:#{message.power} and voltage:#{message.voltage} to influxdb"
-    rescue InfluxDB::Error => e
-      puts e
-    end
+    #puts "#{message.power}"
+    #begin
+      #influxdb.write_point("power", {:value => message.power})
+      #influxdb.write_point("voltage", {:value => message.voltage})
+      #puts "Wrote power:#{message.power} and voltage:#{message.voltage} to influxdb"
+    #rescue InfluxDB::Error => e
+    #  puts e
+    #end
 
     kw_power = message.power / 1000.0
     plotwatt_messages << "#{meter_id},#{kw_power.round(2)},#{Time.new.to_i}"
-    if plotwatt_messages.size >= 60
+    if plotwatt_messages.size >= 30
       puts plotwatt_messages.join(',')
       begin
         response = RestClient.post "http://#{api_key}:@plotwatt.com/api/v2/push_readings", plotwatt_messages.join(',')
